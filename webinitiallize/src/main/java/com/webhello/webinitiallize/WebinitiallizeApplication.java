45c3a822b71854076005d12b44bad5e37ebcb6a7
Christian Salinas <cmsalinas2000@gmail.com>
2025-04-25 23:52:44 -0500

    initilize new hello world project

---
+++ webinitiallize/src/main/java/com/webhello/webinitiallize/WebinitiallizeApplication.java
@@ -0,0 +1,13 @@
+package com.webhello.webinitiallize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebinitiallizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebinitiallizeApplication.class, args);
+	}
+
+}
---
+++ webinitiallize/src/test/java/com/webhello/webinitiallize/WebinitiallizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webhello.webinitiallize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebinitiallizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
